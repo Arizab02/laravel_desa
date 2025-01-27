3e2a97018e5ae082387940b3aaa0fe7ad5f5178d
Arizab02 <arizab002@gmail.com>
2025-01-27 14:16:57 +0700

    components

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,9 @@
 <x-app-layout>
-    
+    {{-- bisa seperti ini --}}
+    {{-- <x-slot:heading> --}}
+    <x-slot name="heading">
+        Home
+    </x-slot>
+    {{-- </x-slot:heading> --}}
     <h1>HOme </h1>
 </x-app-layout>
